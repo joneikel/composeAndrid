8b09c8ded2f856306ebef869667c71dc285e3537
joneikel <joneikel2010@gmail.com>
2025-01-29 22:49:18 -0400

    [ADD] Modificadores

--- app/src/main/java/com/joneikel/cursocompose/MainActivity.kt
+++ app/src/main/java/com/joneikel/cursocompose/MainActivity.kt
@@ -7,30 +7,40 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.joneikel.cursocompose.ui.theme.CursoComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContent {
-            Row {
+            Row(modifier = Modifier.padding(all = 16.dp)) {
+                Spacer(modifier = Modifier.height(50.dp))
                 Image(
                     painter = painterResource(id = R.drawable.fragata),
                     contentDescription = null,
+                    modifier = Modifier.size(50.dp)
 
-                    )
+                )
+                Spacer(modifier = Modifier.width(5.dp))
                 Column {
                     nameCard()
+                    Spacer(modifier = Modifier.height(10.dp))
                     mensageCard(name = "Joneikel")
+
                 }
             }
 
